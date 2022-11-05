0dfa4024990bc9292178c6b970b1036304cb1eaa
midoz82 <FAKKZbuN8YZizS>
2022-11-05 14:01:40 +0100

    initialize project

---
+++ src/test/kotlin/com/yallacode/ParkinglotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yallacode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParkinglotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
