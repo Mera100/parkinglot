d5b6c7340fa1e002c1ea766ec906d1aed7189de6
majdhasan <majd.hasan@hotmail.com>
2022-12-11 10:11:17 +0100

    add ReservationEntity

--- reservation/src/main/kotlin/com/yallacode/reservation/repository/ReservationEntity.kt
+++ reservation/src/main/kotlin/com/yallacode/reservation/repository/ReservationEntity.kt
@@ -2,6 +2,7 @@ package com.yallacode.reservation.repository
 
 import lombok.AllArgsConstructor
 import lombok.NoArgsConstructor
+import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
@@ -11,9 +12,11 @@ import javax.persistence.Table
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-class ReservationEntity {
+class ReservationEntity (
     @Id
     @GeneratedValue
-    var id: Long? = null
-    var startTime:
-}
+    var id: Long? = null,
+    var startTime: LocalDateTime,
+    var endTime: LocalDateTime,
+    var duration: Double?
+    )
