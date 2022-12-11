e5106ae9772791399b34c689620190b508a70abd
majdhasan <majd.hasan@hotmail.com>
2022-12-11 16:15:38 +0100

    extend address and controller get

--- reservation/src/main/kotlin/com/yallacode/reservation/application/controller/ReservationController.kt
+++ reservation/src/main/kotlin/com/yallacode/reservation/application/controller/ReservationController.kt
@@ -1,10 +1,21 @@
 package com.yallacode.reservation.application.controller
 
+import com.yallacode.reservation.application.incoming.SlotQueryDTO
 import com.yallacode.reservation.domain.service.ReservationService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1/reservation")
 class ReservationController(
     val reservationService: ReservationService
 ) {
+
+    @GetMapping("find-available-parkings")
+    fun findAvailableParkingLots(@RequestBody slotQueryDTO: SlotQueryDTO){
+
+
+    }
 }
--- slot/src/main/kotlin/com/yallacode/domain/model/Address.kt
+++ slot/src/main/kotlin/com/yallacode/domain/model/Address.kt
@@ -9,6 +9,7 @@ class Address (
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null,
+    var city: String,
     var longitude: Double,
     var latitude: Double
 )
